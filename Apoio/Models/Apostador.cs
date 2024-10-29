ac5c2e56e50a12789c928b30f66545bfc9987524
LuizHenriqueBarbosaDaSilva <luizbarbosa785@gmail.com>
2024-10-29 20:46:22 -0300

    Fiz um comentario que aprendi na aula!

--- Apoio/Models/Apostador.cs
+++ Apoio/Models/Apostador.cs
@@ -3,8 +3,11 @@
     public class Apostador
     {
         public int Id { get; set; }
+        // [DisplayName("NickName")]
         public string Name { get; set; }
+        //[MaxLength(1000)]
         public string Email { get; set; }
+        // public decimal? Saldo { get; set; } -> Exemplo de nulo
         public decimal Saldo { get; set; }
 
     }
