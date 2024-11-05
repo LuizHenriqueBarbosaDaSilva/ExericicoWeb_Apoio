9e4008b667c710e7889ed867b94e3f45fe7ef77f
LuizHenriqueBarbosaDaSilva <luizbarbosa785@gmail.com>
2024-11-05 20:44:56 -0300

    Feito o aposta Index, Create

---
+++ ExericicoWeb_Apoio-master/Apoio/Models/Aposta.cs
@@ -0,0 +1,13 @@
+﻿namespace Apoio.Models
+{
+    public class Aposta
+    {
+        public int Id { get; set; }
+        public decimal Valor { get; set; }
+        public decimal PrevisaoDeGanho { get; set; }
+        public bool Vencedora { get; set; }
+        public int ApostadorId { get; set; }
+        public Apostador? Apostador { get; set; }
+
+    }
+}
--- ExericicoWeb_Apoio-master/Apoio/Models/Apostador.cs
+++ ExericicoWeb_Apoio-master/Apoio/Models/Apostador.cs
@@ -1,15 +1,12 @@
-﻿using System.Globalization;
-
-namespace Apoio.Models
+﻿namespace Apoio.Models
 {
     public class Apostador
     {
         public int Id { get; set; }
         // [DisplayName("NickName")]
         public string Name { get; set; }
-        //[MaxLength(1000)]
         public string Email { get; set; }
-        // public decimal? Saldo { get; set; } -> Exemplo de nulo
         public decimal Saldo { get; set; }
+
     }
 }
