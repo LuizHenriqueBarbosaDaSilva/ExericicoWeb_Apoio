21c6b766c5f1371763f16940ffb29d85a7c0e927
LuizHenriqueBarbosaDaSilva <luizbarbosa785@gmail.com>
2024-11-19 18:56:50 -0300

    updates

--- Apoio/Controllers/ApostaController.cs
+++ Apoio/Controllers/ApostaController.cs
@@ -28,7 +28,7 @@ namespace Apoio.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Valor", "PrevisaoDeGanho", "Vencendora", "ApostadorId")] Aposta aposta)
+        public async Task<IActionResult> Create([Bind("Valor", "PrevisaoDeGanho", "Vencedora", "ApostadorId")] Aposta aposta)
         {
             if (!ModelState.IsValid) 
             {
@@ -42,5 +42,102 @@ namespace Apoio.Controllers
 
 
         }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var aposta = await _context.Apostas.FindAsync(id);
+            if (aposta == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["ApostadorId"] = new SelectList(_context.Apostadores, "Id", "Name");
+            return View(aposta);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Valor,PrevisaoDeGanha,Vencedora,ApostadorId")] Aposta aposta)
+        {
+            if (id != aposta.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(aposta);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ApostaExists(aposta.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(aposta);
+        }
+        private bool ApostaExists(int id)
+        {
+            return _context.Apostas.Any(e => e.Id == id);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var details = await _context.Apostas.Include(m => m.Apostador).FirstOrDefaultAsync(m => m.Id == id);
+            if (details == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["ApostadorId"] = new SelectList(_context.Apostadores, "Id", "Name");
+            return View(details);
+        }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var delete = await _context.Apostas.Include(m => m.Apostador).FirstOrDefaultAsync(m => m.Id == id);
+            if (delete == null)
+            {
+                return NotFound();
+            }
+
+            return View(delete);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var delete = await _context.Apostas.FindAsync(id);
+            if (delete != null)
+            {
+                _context.Apostas.Remove(delete);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        
     }
 }
